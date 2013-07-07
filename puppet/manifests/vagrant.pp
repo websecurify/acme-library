f7fac6859ac058928bed2f5eb456e46949628fa7
Petko D. Petkov <pdp@aluminum.home>
2013-07-07 16:17:33 +0100

    Additional code cleanup.

--- puppet/manifests/vagrant.pp
+++ puppet/manifests/vagrant.pp
@@ -1,67 +1,81 @@
 class system-update {
-  class { 'apt':
-  }
-
-  exec { 'apt-update':
-    command => '/usr/bin/apt-get update',
-  }
-
-  Exec['apt-update'] -> Package <| |>
+	class { 'apt':
+	}
+	
+	exec { 'apt-update':
+		command => '/usr/bin/apt-get update',
+	}
+	
+	Exec['apt-update'] -> Package <| |>
 }
 
 class mysql-setup {
-  require system-update
-
-  class { 'mysql':
-  }
-
-  class { 'mysql::server':
-    config_hash => {
-      root_password => 'toor'
-    },
-  }
+	class { 'mysql':
+	}
+	
+	class { 'mysql::server':
+		config_hash => {
+			root_password => 'toor',
+		},
+	}
 }
 
 class apache-setup {
-  require system-update
-
-  class { 'apache':
-    mpm_module => 'prefork',
-  }
-
-  class { 'apache::mod::proxy_http':
-  }
-
-  apache::vhost { 'acme-library':
-    port => '80',
-    docroot => '/vagrant/app',
-	proxy_pass => [ { path => '/', url => 'http://localhost:8080/acme-library/' } ],
-	default_vhost => true,
-  }
+	class { 'apache':
+		mpm_module => 'prefork',
+	}
+	
+	class { 'apache::mod::proxy_http':
+	}
+	
+	apache::vhost { 'acme-library':
+		default_vhost => true,
+		port => '80',
+		docroot => '/vagrant/app',
+		
+		proxy_pass => [
+			{
+				path => '/',
+				url => 'http://localhost:8080/acme-library/',
+			},
+		],
+	}
 }
 
 class tomcat-setup {
-  require system-update
+	package { 'tomcat7':
+		ensure => installed,
+	}
+	
+	file { '/var/lib/tomcat7/webapps/acme-library':
+		require => Package['tomcat7'],
+		ensure => link,
+		target => '/vagrant/app/target/webapp',
+	}
+	
+	service { 'tomcat7':
+		require => Package['tomcat7'],
+		ensure => running,
+	}
+}
 
-  package { 'tomcat7':
-    ensure => installed
-  }
+class maven-setup {
+	package { 'maven2':
+		ensure => installed,
+	}
+}
 
-  file { '/var/lib/tomcat7/webapps/acme-library':
-    require => Package['tomcat7'],
-
-    ensure => link,
-	target => '/vagrant/app/target/webapp',
-  }
-
-  service { 'tomcat7':
-    require => Package['tomcat7'],
-
-    ensure => running,
-  }
+class app-setup {
+	exec { 'app-rebuild':
+		require => Package['maven2'],
+		command => '/usr/bin/mvn war:exploded',
+		cwd => '/vagrant/app',
+	}
 }
 
 include system-update
 include mysql-setup
 include apache-setup
 include tomcat-setup
+include maven-setup
+include app-setup
